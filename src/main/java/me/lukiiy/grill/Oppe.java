98eb8143beeb38709c128ed03fc3faaffed58ea1
Lukiiy <lukiiy@proton.me>
2026-05-10 09:12:33 -0300

    Reworked Oppe
    * Reworked entityItemPersistent to use UUIDs
    * Removed object persistency for entityItemPersistent
    * Reworked how it nerfs spawner entities, which now checks for a "target" nearby
    * Reworked adjustRenderDist to now take care about simulation distance

--- src/main/java/me/lukiiy/grill/Oppe.java
+++ src/main/java/me/lukiiy/grill/Oppe.java
@@ -2,7 +2,6 @@ package me.lukiiy.grill;
 
 import com.destroystokyo.paper.entity.ai.MobGoals;
 import com.destroystokyo.paper.entity.ai.VanillaGoal;
-import com.destroystokyo.paper.event.entity.EntityRemoveFromWorldEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.*;
@@ -10,15 +9,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.entity.EntityRemoveEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 public class Oppe implements Listener {
     private final MobGoals goalsManager = Bukkit.getServer().getMobGoals();
-    private final Set<LivingEntity> entityItemPersistent = new HashSet<>();
+    private final Set<UUID> entityItemPersistent = new HashSet<>();
 
     @EventHandler
     public void spawn(CreatureSpawnEvent e) {
@@ -38,7 +39,24 @@ public class Oppe implements Listener {
             entity.setCanPickupItems(false);
             entity.setPersistent(false);
             entity.setRemoveWhenFarAway(true);
-            entity.setJumping(false);
+
+            entity.getScheduler().runDelayed(Grill.getInstance(), (t) -> {
+                if (!entity.isValid()) return;
+
+                boolean active = false;
+                EntityType type = entity.getType();
+
+                for (Entity nearby : entity.getNearbyEntities(8, 4, 8)) {
+                    if (!(nearby instanceof LivingEntity living) || nearby == entity || nearby.getType() == type || !entity.hasLineOfSight(living)) continue;
+
+                    active = true;
+                    break;
+                }
+
+                entity.setAI(active);
+                entity.setAware(active);
+                entity.setJumping(active);
+            }, null, 1L);
         }
     }
 
@@ -48,14 +66,18 @@ public class Oppe implements Listener {
 
         if (entity.getSpawnCategory() != SpawnCategory.MONSTER) return;
 
-        Bukkit.getScheduler().runTaskLater(Grill.getInstance(), () -> {
-            if (entity.isPersistent()) entityItemPersistent.add(entity);
-        }, 2L);
+        entity.getScheduler().runDelayed(Grill.getInstance(), (t) -> {
+            if (entity.isPersistent()) entityItemPersistent.add(entity.getUniqueId());
+        }, null, 2L);
     }
 
     @EventHandler
-    public void mobDespawnAttempt(EntityRemoveFromWorldEvent e) {
-        if (!(e.getEntity() instanceof LivingEntity living) || !entityItemPersistent.contains(living) || living.getEquipment() == null) return;
+    public void mobDespawnAttempt(EntityRemoveEvent e) {
+        if (!(e.getEntity() instanceof LivingEntity living) || !entityItemPersistent.contains(living.getUniqueId())) return;
+
+        entityItemPersistent.remove(living.getUniqueId());
+
+        if (living.getEquipment() == null) return;
 
         living.setHealth(0);
     }
@@ -71,12 +93,15 @@ public class Oppe implements Listener {
     }
 
     private void adjustRenderDist(Player p) {
-        int dist = Math.min(p.getWorld().getViewDistance(), p.getClientViewDistance());
-        if (dist < 4) return;
+        World w = p.getWorld();
 
-        if (p.getWorld().getEnvironment() == World.Environment.NETHER) dist -= 2;
-        if (p.getWorld().getEnvironment() == World.Environment.THE_END) dist -= 1;
+        int offset = switch (w.getEnvironment()) {
+            case NETHER -> 2;
+            case THE_END -> 1;
+            default -> 0;
+        };
 
-        p.setSendViewDistance(dist);
+        p.setSendViewDistance(Math.max(4, Math.min(w.getViewDistance(), p.getClientViewDistance()) - offset));
+        p.setSimulationDistance(Math.max(3, Math.min(w.getSimulationDistance(), p.getSimulationDistance()) - offset));
     }
 }
