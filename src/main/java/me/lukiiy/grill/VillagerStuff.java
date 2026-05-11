8869769fc96a35a9d4389d75f6f88242fe41e585
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-05-11 13:18:17 -0300

    Best reputation in a Villager is now shared to others

--- src/main/java/me/lukiiy/grill/VillagerStuff.java
+++ src/main/java/me/lukiiy/grill/VillagerStuff.java
@@ -1,11 +1,15 @@
 package me.lukiiy.grill;
 
+import com.destroystokyo.paper.entity.villager.Reputation;
+import com.destroystokyo.paper.entity.villager.ReputationType;
 import org.bukkit.Difficulty;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class VillagerStuff implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
@@ -17,5 +21,33 @@ public class VillagerStuff implements Listener {
         villager.getScheduler().run(Grill.getInstance(), (_) -> {
             if (!villager.isDead()) villager.zombify();
         }, null);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void shareBestRep(PlayerInteractEntityEvent e) { // WIP? - Compartilha a melhor reputação positiva existente para quem interagiu
+        if (!(e.getRightClicked() instanceof Villager villager)) return;
+
+        Player player = e.getPlayer();
+        if (villager.getReputations().isEmpty()) return;
+
+        Reputation best = null;
+        int bestScore = 0;
+
+        for (Reputation rep : villager.getReputations().values()) {
+            int score = rep.getReputation(ReputationType.MAJOR_POSITIVE) + rep.getReputation(ReputationType.MINOR_POSITIVE);
+
+            if (best == null || score > bestScore) {
+                bestScore = score;
+                best = rep;
+            }
+        }
+
+        if (best == null) return;
+
+        Reputation playerRep = villager.getReputation(player.getUniqueId());
+
+        playerRep.setReputation(ReputationType.MAJOR_POSITIVE, best.getReputation(ReputationType.MAJOR_POSITIVE));
+        playerRep.setReputation(ReputationType.MINOR_POSITIVE, best.getReputation(ReputationType.MINOR_POSITIVE));
+        villager.setReputation(player.getUniqueId(), playerRep);
     }
 }
