184d4590f0271d46d83059dbc267f7b587407259
Lukiiy <lukiiy@proton.me>
2026-07-18 18:13:21 -0300

    Add isBoxed

--- src/main/java/me/lukiiy/grill/VillagerStuff.java
+++ src/main/java/me/lukiiy/grill/VillagerStuff.java
@@ -3,6 +3,9 @@ package me.lukiiy.grill;
 import com.destroystokyo.paper.entity.villager.Reputation;
 import com.destroystokyo.paper.entity.villager.ReputationType;
 import org.bukkit.Difficulty;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
@@ -12,6 +15,8 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class VillagerStuff implements Listener {
+    private static final BlockFace[] HORIZONTAL = { BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST };
+
     @EventHandler(priority = EventPriority.LOWEST)
     public void villagerDmg(EntityDamageEvent e) { // Força Villagers para serem zumbificados mesmo sem estar na dificuldade Hard
         if (!(e.getEntity() instanceof Villager villager) || villager.getWorld().getDifficulty() == Difficulty.HARD || e.getFinalDamage() <= villager.getHealth()) return;
@@ -49,5 +54,22 @@ public class VillagerStuff implements Listener {
         playerRep.setReputation(ReputationType.MAJOR_POSITIVE, best.getReputation(ReputationType.MAJOR_POSITIVE));
         playerRep.setReputation(ReputationType.MINOR_POSITIVE, best.getReputation(ReputationType.MINOR_POSITIVE));
         villager.setReputation(player.getUniqueId(), playerRep);
+    }
+
+    private boolean isBoxed(Villager villager) {
+        World world = villager.getWorld();
+
+        int x = villager.getLocation().getBlockX();
+        int y = villager.getLocation().getBlockY();
+        int z = villager.getLocation().getBlockZ();
+
+        for (BlockFace face : HORIZONTAL) {
+            Block feet = world.getBlockAt(x + face.getModX(), y, z + face.getModZ());
+            Block head = world.getBlockAt(x + face.getModX(), y + 1, z + face.getModZ());
+
+            if (feet.isPassable() && head.isPassable()) return false;
+        }
+
+        return true;
     }
 }
